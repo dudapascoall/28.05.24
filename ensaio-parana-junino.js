6ec2ac89f8131678f3408107a9058d8933efa6c0
dudapascoall <142435751+dudapascoall@users.noreply.github.com>
2024-05-29 10:51:11 -0300

    Add files via upload

---
+++ ensaio-parana-junino.js
@@ -0,0 +1,31 @@
+class Pessoa{
+    constructor(nome,movimento, audicao, toque_fisico, cheiro, sabor, visao, tamanho, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexiblidade, forca, voz, mao, pe) {
+        this.nome = nome;
+        this.movimento = movimento;
+        this.idade = idade;
+        this.cor = cor;
+        this.genero = genero;
+        this.colaboraçao = colaboracao;
+        this.equilibrio = equilibrio;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
+var pessoa = [];
+pessoa[0] = new Pessoa(ana, movimento, 15, branca, feminino, colaboracao, equilibrio, 1,68, 58);
+pessoa[1] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[2] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[3] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[4] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[5] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[6] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[7] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[8] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[9] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[10] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[11] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[12] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[13] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[14] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[15] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
+pessoa[16] = new Pessoa(bernardo, movimento, 16, branco, masculino, colaboracao, equilibrio, , );
